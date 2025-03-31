22590b36a6cb6153712ebb62925e19a2329cbff7
João Pedro Bandeira <jbandeira@detro.rj.gov.br>
2025-03-31 15:54:55 -0300

    Refactor SmartTv class by removing main method, adding state management methods, and introducing Usuario class for testing functionality

--- estudos/src/SmartTv.java
+++ estudos/src/SmartTv.java
@@ -1,30 +1,29 @@
 public class SmartTv {
-    public static void main(String[] args) {
-        SmartTv smartTv = new SmartTv();
-        System.out.println("TV Ligada? " + smartTv.mudarEstado(true));
-        System.out.println("Volume atual: " + smartTv.aumentarVolume(50));
-        System.out.println("Canal atual: " + smartTv.aumentarCanal(1));
-        System.out.println("Canal atual: " + smartTv.diminuirCanal(1)); 
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
     }
     
-    public boolean mudarEstado(boolean estado) {
-        boolean estadoFinal = !estado;
-        return estadoFinal;
+    public void aumentarCanal() {
+        canal++;
     }
 
-    public int aumentarVolume(int volume) {
-        return volume + 1;
-    }
-
-    public int diminuirVolume(int volume) {
-        return volume - 1;
-    }
-    
-    public int aumentarCanal(int canal) {
-        return canal + 1;
-    }
-
-    public int diminuirCanal(int canal) {
-        return canal - 1;
+    public void diminuirCanal() {
+        canal--;
     }
 }
---
+++ estudos/src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.ligar();
+        System.out.println("TV Ligada? " + smartTv.ligada);
+    }
+}
